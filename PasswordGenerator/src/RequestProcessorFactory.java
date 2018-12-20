3d00e28a65167c7a5fb71f9f8a283cfc153e3673
Ulrich Dah-Achinanon <ulrich.dah-achinanon@polymtl.ca>
2018-12-20 12:18:38 -0500

    Created method for processor creation

--- PasswordGenerator/src/RequestProcessorFactory.java
+++ PasswordGenerator/src/RequestProcessorFactory.java
@@ -1,7 +1,17 @@
 
 public class RequestProcessorFactory {
-	public RequestProcessor createRequestProcessor(int mode) {
-		return null;
+	public RequestProcessor createRequestProcessor(int mode) throws Exception {
+		
+		switch(mode) {
+		case 1:
+			return new RetrievalProcessor();
+		
+		case 2:
+			return new GenerationProcessor();
+		
+		default:
+			throw new Exception("This option do not exist");
+		}
 		
 	}
 }
